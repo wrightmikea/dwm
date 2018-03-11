c65b13f21576a09bdaf63a3f16d4edd501fbda40
Mike Wright <wrightmikea@gmail.com>
2018-03-11 12:36:43 -0700

    add chromium, emacs rules

--- config.def.h
+++ config.def.h
@@ -29,6 +29,8 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Emacs",    NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Chromium", NULL,       NULL,       1 << 2,       0,           -1 },
 };
 
 /* layout(s) */
